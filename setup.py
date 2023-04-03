f5c001bf4854d96e5e0c22005b4298944c3cc46e
tumble-weed <aniketsinghresearch@gmail.com>
2023-04-03 08:26:03 +0000

    hacks for easier debugging

---
+++ setup.py
@@ -0,0 +1,13 @@
+import builtins
+import ipdb
+builtins.ipdb = ipdb
+import importlib
+builtins.importlib = importlib
+import sys
+builtins.sys = sys
+import os
+builtins.os = os
+import torch
+builtins.torch = torch
+import numpy as np
+builtins.np = np
